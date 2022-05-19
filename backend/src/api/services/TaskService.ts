07bad27be8c9fafe322e17a5d013f3ae27e575c7
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-05-19 12:08:50 -0300

    feat: implements taskService

---
+++ backend/src/api/services/TaskService.ts
@@ -0,0 +1,11 @@
+import { Task } from "../../database/interfaces/TaskInterface";
+import Service from "./IndexService";
+import TaskModel from "../../database/models/TaskModel";
+
+class TaskService extends Service<Task> {
+  constructor(public model = new TaskModel()) {
+    super(model);
+  }
+}
+
+export default TaskService;
