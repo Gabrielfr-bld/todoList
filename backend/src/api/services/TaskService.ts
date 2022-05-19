135d2cd0ae5659cbe7539a13f858112546b04499
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-05-19 14:30:45 -0300

    feat: implements create in task service

--- backend/src/api/services/TaskService.ts
+++ backend/src/api/services/TaskService.ts
@@ -7,6 +7,11 @@ class TaskService extends Service<Task> {
   constructor(public model = new TaskModel()) {
     super(model);
   }
+  public async create(task: Task): Promise<Task> {
+    const validation = TaskSchema.safeParse(task);
+    if(!validation.success) throw new InvalidFieldsError();
+    return this.model.create(task);
+  }
 }
 
 export default TaskService;
