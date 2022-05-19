b1037fa3de1293fbc6cd3eac9a50ff345e32cb96
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-05-19 14:27:16 -0300

    feat: implements class InvalidFieldError

--- backend/src/api/services/TaskService.ts
+++ backend/src/api/services/TaskService.ts
@@ -1,6 +1,7 @@
 import { Task, TaskSchema } from "../../database/interfaces/TaskInterface";
 import Service from "./IndexService";
 import TaskModel from "../../database/models/TaskModel";
+import { InvalidFieldsError } from "../errors";
 
 class TaskService extends Service<Task> {
   constructor(public model = new TaskModel()) {
