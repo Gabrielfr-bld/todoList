02f8fce5a260c5114c7b0ad0981e75a52da106e6
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-05-19 12:03:58 -0300

    feat: implements taskInterface

---
+++ backend/src/database/interfaces/TaskInterface.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const TaskSchema = z.object({
+  task: z.string({
+    required_error: 'Task is required',
+    invalid_type_error: 'Task must be a string',
+  }).min(3, { message: 'Task must be 3 or more characters long' }),
+  status: z.enum(['pendente', 'em andamento', 'pronto']),
+  createdAt: z.date()
+})
+
+export type Task = z.infer<typeof TaskSchema>;
+
+export { TaskSchema };
